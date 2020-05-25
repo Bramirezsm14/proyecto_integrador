fa1dd67b8d61c444c1634a1e087aacf8ea081c9a
Brian <bramirezsm14@gmail.com2>
2020-05-25 18:54:36 -0300

    final

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,22 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req,res,next) {
-  res.render('index', { title: 'Unlimited Sport' });
+router.get('/', function(req,res) {
+  res.render('home',{ title: 'Unlimited Sport' });
   
 });
+
+router.get('/login',function(req,res){
+  res.render('form')
+});
+
+router.get('/check',function(req,res){
+  res.render('checkOut')
+});
+
+router.get('/product',function(req,res){
+  res.render('detalleProducto')
+});
+ 
 
 module.exports = router;
